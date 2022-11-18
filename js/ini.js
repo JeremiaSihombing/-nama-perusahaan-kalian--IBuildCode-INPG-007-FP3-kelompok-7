9d895c3537620bfe4e344726821d18acc9734460
nadia21r <nadiaristiani212@gmail.com>
2022-11-18 19:47:46 +0700

    benerin button dan see our work yang bawah

---
+++ js/ini.js
@@ -0,0 +1,4 @@
+var butSee = document.getElementById("butSeeWork")
+butSee.addEventListener("click", function(event) {
+    alert("Please click the 'SEE OUR WORK' button after this, if you want to see our company's projects, thank you!")
+})
