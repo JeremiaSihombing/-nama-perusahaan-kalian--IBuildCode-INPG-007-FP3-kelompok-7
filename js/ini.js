56f9176885c75e3f322f2021c8deecbd108e6ffe
nadia21r <nadiaristiani212@gmail.com>
2022-11-19 10:30:18 +0700

    benerin button see our work kedua

--- js/ini.js
+++ js/ini.js
@@ -1,8 +1,3 @@
-var butSee = document.getElementById("butSeeWork")
-butSee.addEventListener("click", function(event) {
-    alert("Please click the 'SEE OUR WORK' button after this, if you want to see our company's projects, thank you!")
-})
-
 var butMess = document.getElementById("butMess")
 butMess.addEventListener("click", function(event) {
     var email = $("#inEmail").val()
